1ec3404eac69b400dee518ad4287d70b025fc6ca
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-13 16:58:41 -0300

    Create a token verify

--- src/API/routes.ts
+++ src/API/routes.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response} from "express"
 const router = Router();
 import connectDB from "../data/config";
 import user from "../data/model";
+import jwt from 'jsonwebtoken';
 
 router.post('/logon', (req: Request, res: Response) => {
     connectDB();
@@ -17,7 +18,20 @@ router.post('/login',  async (req: Request, res: Response) => {
     console.log("Req recebida!")
     const check = await user.findOne({'email': email, 'password': password});
     if (check){
-        res.status(200).send("Account exists!");
+        const token = jwt.sign(
+            {user: check},
+            'meusegredo',
+            {expiresIn: '1h'}
+        )
+        res.status(200).json(
+            {message: 'Success!',
+                token,
+                user: {
+                    email: email,
+                    name: check.email
+                }
+            }
+        );
     }
     else {
         res.status(400).send("Account does not exists!");
