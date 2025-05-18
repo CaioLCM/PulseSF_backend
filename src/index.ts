179bb4fdda6d63bb9f4da964ca8b8dda23b1980c
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-18 11:27:56 -0300

    Add the cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './API/routes';
 import "dotenv/config"
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(router);
