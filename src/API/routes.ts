887b6024ea4a278c7fdc1c030a8186e970c9cb77
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-06-05 18:05:13 -0300

    Now it's possible to add friends and remove friends request

--- src/API/routes.ts
+++ src/API/routes.ts
@@ -11,7 +11,37 @@ import { connect } from "http2";
 import { json } from "stream/consumers";
 import { strict } from "assert";
 
-connectDB()
+connectDB();
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  console.log("Req recebida!");
+  const check = await user.findOne({ email: email, password: password });
+  if (check) {
+    const token = jwt.sign(
+      {
+        user: {
+          email: check.email,
+          nickname: check.username,
+          profilePicture: check.profilePicture,
+        },
+      },
+      process.env.JWT_SECRET!,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      message: "Success!",
+      token,
+      user: {
+        email: email,
+        name: check.username,
+        profile_picture: check.profilePicture,
+      },
+    });
+  } else {
+    res.status(400).send("Account does not exists!");
+  }
+});
 
 router.post("/logon", async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
@@ -32,7 +62,6 @@ router.post("/logon", async (req: Request, res: Response) => {
     res.status(400).send("Not possible to create the account!");
   }
 });
-
 
 router.post("/profileAdd", async (req: Request, res: Response) => {
   const { email, image } = req.body;
@@ -82,151 +111,212 @@ router.post("/searchPicture", async (req: Request, res: Response) => {
 });
 
 router.post("/addMember", async (req: Request, res: Response): Promise<any> => {
-  const {project_name, member_list} = req.body;
+  const { project_name, member_list } = req.body;
   const check = await project.updateOne(
     {
       projectName: project_name,
     },
     {
       $set: { member_list: member_list },
-    },
+    }
   );
-  
-  if (check.matchedCount == 0){
-    return res.status(404).json({message: "Project not found"});
+
+  if (check.matchedCount == 0) {
+    return res.status(404).json({ message: "Project not found" });
   }
-  
-  if(check.modifiedCount > 0 || (check.matchedCount > 0 && check.modifiedCount === 0)){
-    res.status(200).json({message: "Updated member list!"})
+
+  if (
+    check.modifiedCount > 0 ||
+    (check.matchedCount > 0 && check.modifiedCount === 0)
+  ) {
+    res.status(200).json({ message: "Updated member list!" });
+  } else {
+    return res.status(400).json({ message: "Not possible to update the list" });
   }
-  
-  else {
-    return res.status(400).json({message: "Not possible to update the list"})
-  }
-  
 });
 
 router.get("/searchUsers", async (req, res) => {
   const check = await user.find({});
   const users_info: { email: string; profile_picture: string }[] = [];
-  check.map((pr) => users_info.push({ email: pr["email"], profile_picture: pr["profilePicture"] != null? pr["profilePicture"] : "No profile picture" }));
-  res.status(200)
-  .json(
-    {
-      users_info
-    }
-  )
-})
+  check.map((pr) =>
+    users_info.push({
+      email: pr["email"],
+      profile_picture:
+        pr["profilePicture"] != null
+          ? pr["profilePicture"]
+          : "No profile picture",
+    })
+  );
+  res.status(200).json({
+    users_info,
+  });
+});
 
 router.post("/searchUser", async (req, res) => {
-  console.log("Search user working!!!")
-  console.log(req.body['email'])
+  console.log("Search user working!!!");
+  console.log(req.body["email"]);
   const check = await user.findOne({
-    email: req.body['email']
-  })
+    email: req.body["email"],
+  });
   if (check) {
-    const bio = check.get("bio") == null ? "Add your info here!!!" : check.get("bio")
+    const bio =
+      check.get("bio") == null ? "Add your info here!!!" : check.get("bio");
     res.status(200).json({
-      "bio": bio 
+      bio: bio,
     });
   } else {
     console.log("User not found");
     res.status(400);
   }
-})
+});
 
 router.post("/updateBio", async (req, res) => {
-  const {email, bio} = req.body;
-  const check = await user.updateOne({
-    email: email
-  },
-  
-  {
-    $set: {bio: bio}
-  },
-  {new: true}
-);
-console.log(check.matchedCount)
-res.status(200).json({ message: "Bio updated!" });
+  const { email, bio } = req.body;
+  const check = await user.updateOne(
+    {
+      email: email,
+    },
+
+    {
+      $set: { bio: bio },
+    },
+    { new: true }
+  );
+  console.log(check.matchedCount);
+  res.status(200).json({ message: "Bio updated!" });
 });
+
+///////////////////////////////////////////////////////////
 
 router.post("/addFriend", async (req, res) => {
   const requests = [];
-  const {email_req, email_res} = req.body;
-  
-  try{
+  const { email_req, email_res } = req.body;
+
+  try {
     const previous = await user.findOne({
-      email: email_res
-    })
-    
-    const previous_requests = previous?.get("add_request")
-    
-    if(previous_requests != null){
-      previous_requests.forEach((add_req) => {
-        requests.push(add_req)
-      })
-    }
-    
-    requests.push(email_req)
-    
-    const check = await user.updateOne({
-      email: email_res
-    },
-    {
-      $set: {add_request: requests}
-    }
-  )
-  res.status(200)
-} catch (erro: any) {
-  console.log("Error! " + erro)
-  res.status(400)
-}
-
-})
-
-router.post("/checkFriend", async (req: Request, res: Response) => {
-  const {email_req, email_res} = req.body
-  const check = await user.findOne({
-    email: email_res
-  })
-  console.log(check?.email)
-  check?.add_request?.forEach((add_req) => {
-    if (add_req == email_req){
-      res.status(200).end()
-    }
-  })
-
-  res.status(400).end()
-}
-)
-
-router.post("/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  console.log("Req recebida!");
-  const check = await user.findOne({ email: email, password: password });
-  if (check) {
-    const token = jwt.sign(
-      {
-        user: {
-          email: check.email,
-          nickname: check.username,
-          profilePicture: check.profilePicture,
-        },
-      },
-      process.env.JWT_SECRET!,
-      { expiresIn: "1h" }
-    );
-    res.status(200).json({
-      message: "Success!",
-      token,
-      user: {
-        email: email,
-        name: check.username,
-        profile_picture: check.profilePicture,
-      },
+      email: email_res,
     });
-  } else {
-    res.status(400).send("Account does not exists!");
+
+    const previous_requests = previous?.get("add_request");
+
+    if (previous_requests != null) {
+      previous_requests.forEach((add_req) => {
+        requests.push(add_req);
+      });
+    }
+
+    requests.push(email_req);
+
+    const check = await user.updateOne(
+      {
+        email: email_res,
+      },
+      {
+        $set: { add_request: requests },
+      }
+    );
+    res.status(200);
+  } catch (erro: any) {
+    console.log("Error! " + erro);
+    res.status(400);
   }
 });
+
+router.post("/checkFriend", async (req: Request, res: Response) => {
+  const { email_req, email_res } = req.body;
+  const check = await user.findOne({
+    email: email_res,
+  });
+  check?.add_request?.forEach((add_req) => {
+    if (add_req == email_req) {
+      res.status(200).end();
+    }
+  });
+
+  res.status(400).end();
+});
+
+router.post("/searchRequests", async (req: Request, res: Response) => {
+  const { email } = req.body;
+  const check = await user.findOne({
+    email: email,
+  });
+
+  if (check?.add_request != null) {
+    res.status(200).json({
+      requests: check.add_request,
+    });
+  } else {
+    res.status(400).end();
+  }
+});
+
+router.post("/removeFriendRequest", async (req: Request, res: Response) => {
+  const { email_request, email_response } = req.body;
+  const check = await user.findOne({
+    email: email_response,
+  });
+  if (check?.add_request != null) {
+    const req_list = check.add_request;
+    req_list.splice(req_list.indexOf(email_request), 1);
+    const check_ = await user.findOneAndUpdate(
+      {
+        email: email_response,
+      },
+      {
+        add_request: req_list,
+      }
+    );
+  }
+});
+
+router.post("/acceptFriendRequest", async (req, res) => {
+  const { email_req, email_res } = req.body;
+  const friends1 = [];
+  const user1 = await user.findOne({
+    email: email_req,
+  });
+  if (user1?.friends != null) {
+    user1.friends.forEach((friend) => {
+      friends1.push(friend);
+    });
+  }
+  friends1.push(email_res);
+  await user.findOneAndUpdate(
+    {
+      email: email_req,
+    },
+    { friends: friends1 }
+  );
+
+  const friends2 = [];
+  const user2 = await user.findOne({
+    email: email_res,
+  });
+  if (user2?.friends != null) {
+    user2.friends.forEach((friend) => {
+      friends2.push(friend);
+    });
+  }
+  friends2.push(email_req);
+  await user.findOneAndUpdate(
+    {
+      email: email_res,
+    },
+    { friends: friends2 }
+  );
+  res.status(200);
+});
+
+router.post("/searchFriends", async (req, res) => {
+  console.log("chegou aqui")
+  const { email } = req.body;
+  const foundUser = await user.findOne({ email });
+  // You can now use foundUser to return friends or any other info
+  console.log(foundUser?.friends)
+  res.status(200).json({ friends: foundUser?.friends ?? [] });
+})
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 export default router;
